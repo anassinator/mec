73966903d1ef8646b34a4c46fc7fc397c1148c27
Bei Chen Liu <bei.liu@mail.mcgill.ca>
2015-11-07 15:26:39 -0500

    initial code for ros

--- catkin_ws/src/mec/firmware/main.cpp
+++ catkin_ws/src/mec/firmware/main.cpp
@@ -2,22 +2,38 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <mec/Command.h>
-//#include "motor.h"
+#include <Servo.h>
 
 #define LED_PIN 13
-#define IN1_PIN 3
-#define IN2_PIN 5
-#define IN3_PIN 6
-#define IN4_PIN 9
+#define IN1_PIN 2
+#define IN2_PIN 3
+#define IN3_PIN 4
+#define IN4_PIN 5
+#define SERVO_PIN 6
+#define COMMAND_TIMEOUT 500
+
+unsigned long last_command = 0;
+Servo servo;
 
 ros::NodeHandle nh;
 
-void commandCallback( const mec::Command& msg){
-  if(msg.data > 0){
-
-  }
-  //digital()
+void stopMotors() {
+    digitalWrite(IN1_PIN,LOW);
+    digitalWrite(IN2_PIN,LOW);
+    digitalWrite(IN3_PIN,LOW);
+    digitalWrite(IN4_PIN,LOW);
 }
+
+void commandCallback( const mec::Command& msg){
+  digitalWrite(LED_PIN,!digitalRead(LED_PIN));
+  last_command = micros();
+  digitalWrite(IN1_PIN,msg.left_velocity > 0);
+  analogWrite(IN2_PIN,msg.left_velocity & 0xFF);
+  digitalWrite(IN3_PIN,msg.right_velocity > 0);
+  analogWrite(IN4_PIN,msg.right_velocity & 0xFF);
+  servo.write(msg.servo);
+}
+
 ros::Subscriber<mec::Command> commandSub("/controls",&commandCallback);
 
 void setup() {
@@ -27,6 +43,8 @@ void setup() {
   pinMode(IN3_PIN,OUTPUT);
   pinMode(IN4_PIN,OUTPUT);
 
+  servo.attach(SERVO_PIN);
+
   nh.initNode();
   nh.subscribe(commandSub);
 
@@ -34,5 +52,6 @@ void setup() {
 
 void loop() {
   nh.spinOnce();
-  digitalWrite(LED_PIN,!digitalRead(LED_PIN));
+  if(last_command < micros() + COMMAND_TIMEOUT){
+  }
 }
